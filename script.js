6bce351d7ba2be533ca65c2e76d84459e3f9f80f
nikolatrenkoski <slutatjata1337@gmail.com>
2021-12-26 01:22:23 +0100

    add fizzbuss

---
+++ script.js
@@ -0,0 +1,8 @@
+const user = prompt("enter number");
+
+for (let i = 1; i <= user; i++) {
+  if (i % 5 === 0 && i % 3 === 0) console.log("fizzbuzz");
+  else if (i % 5 === 0) console.log("buzz");
+  else if (i % 3 === 0) console.log("fizz");
+  else console.log(i);
+}
